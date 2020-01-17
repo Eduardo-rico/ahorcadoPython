4ac98e1500e55af8e362c21935334c2c473b9676
Eduardo-rico <emrs94@gmail.com>
2020-01-17 17:56:02 -0600

    se añade la logica del juego, el juego está completo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 
 
-IMGES = ['''
+IMAGES = ['''
     +---+
     |   |
         |
@@ -74,15 +74,46 @@ WORDS = ["lavadora",
 def random_word():
     #funcion que extrae una palabra al azar
     #obtener un indicie con un numero aleatorio
-    indice_aleatorio = random.randint(0, len(WORDS)-1)
-    return WORDS[indice_aleatorio]
+    random_index = random.randint(0, len(WORDS)-1)
+    return WORDS[random_index]
+def display_board(hidden_word, tries):
+    print(IMAGES[tries])
+    print("")
+    print(hidden_word)
+    print("--- * --- * --- * --- * --- --- * --- * --- * --- * --- ")
 
 def run():
     word = random_word()
     hidden_word = ["-"] * len(word)
     tries = 0
+    while True:
+        display_board(hidden_word, tries)
+        current_letter = str(input("Escoge una letra: "))
+        letter_indexes = [] #aqui se guardan todos los indices
+        for letter in range(len(word)):
+            if word[letter] == current_letter:
+                letter_indexes.append(letter)
+        if len(letter_indexes) == 0:
+            tries += 1
+            if tries == 7:
+                display_board(hidden_word, tries)
+                print("")
+                print("¡Perdiste! La palabra correcta era {}".format(word))
+                break
+        else:
+            for index in letter_indexes:
+                hidden_word[index] = current_letter
+            letter_indexes = []
+        try:
+        #si no encuentra el valor .index() arroja un error, si sí lo encuentra no pasa nada, si no lo encuentra regresa un error
+            hidden_word.index("-")
+        except ValueError:
+            #si hay un error, mandamos el mensaje de que has ganado
+            print("")
+            print("Ganaste! La palabra correcta era {}".format(word))
+            break
     
-    pass
+
 
 
 
